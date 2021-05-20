306495860bcb7c8bb83b067cd462cac080af9eca
young ho's notebook <jkijki1249@gmail.com>
2021-05-20 16:56:41 +0900

    .

--- Backjoon/src/BackJoon4673.java
+++ Backjoon/src/BackJoon4673.java
@@ -1,15 +1,48 @@
+import java.util.ArrayList;
 
 public class BackJoon4673 {
 
 	public static void main(String[] args) {
+		
+		ArrayList<Integer> arr = new ArrayList<Integer>(10000);
+		int cnt = 0;
+		while(cnt < 10000) {
+			arr.add(cnt);
+			cnt++;
+		}
+		int num = 1;
 
-		SelfNumber();
+		while (num < 10000) {
+			
+			SelfNumber(num, arr);
+			num++;
+		}
+		cnt = 1;
+		while(cnt < 10000) {
+			if(arr.get(cnt) == cnt) {
+				System.out.println(cnt);
+			}
+			cnt++;
+		}
 	}
-	
-	public static void SelfNumber() {
+
+	public static void SelfNumber(int num, ArrayList<Integer> arr) {
+		int result = 0;
+		if(num < 10) {
+			result = num + num%10;
+			
+		}else if(num < 100) {
+			result = num + num%10+(num/10)%10;
+		}else if(num < 1000) {
+			result = num + num%10+(num/10)%10 + (num/100)%10;
+		}else{
+			result = num + num%10 +(num/10)%10 + (num/100)%10 + (num/1000)%10; 
+		}
+		if(result < 10000) {
+			arr.set(result, -1);
+			SelfNumber(result, arr);
+		}
 		
-		
-		System.out.println();
-		
+
 	}
 }

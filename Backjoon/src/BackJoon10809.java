cdfa96c61c17446acf8a1e423261d73d4cf8c555
young ho's notebook <jkijki1249@gmail.com>
2021-05-24 14:29:10 +0900

    commit

--- Backjoon/src/BackJoon10809.java
+++ Backjoon/src/BackJoon10809.java
@@ -1,9 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
 
 public class BackJoon10809 {
 
 	public static void main(String[] args) {
 
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		int cnt = 0;
+		while(cnt < 26) {
+			
+			arr.add(-1);
+			
+			cnt++;
+		}
 		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		cnt = 0;
+		
+		while(cnt < str.length()) {
+			char a = str.charAt(cnt);
+			
+			if(arr.get(a-'a') == -1) {
+				arr.set(a - 'a', cnt);
+			}
+			
+			cnt++;
+		}
+		
+		cnt = 0;
+		
+		while(cnt < arr.size()) {
+			System.out.println(arr.get(cnt));
+			
+			cnt++;
+		}
+		
+		
+		sc.close();
 
 	}
 
---
+++ Backjoon/src/BackJoon2675.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BackJoon2675 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int test_num = sc.nextInt();
+		
+		int cnt = 0;
+		
+		ArrayList<Integer> repeat_num = new ArrayList<Integer>();
+		ArrayList<String> repeat_text = new ArrayList<String>();
+		
+		while(cnt < test_num) {
+			
+			repeat_num.add(sc.nextInt());
+			repeat_text.add(sc.next());
+			cnt++;
+		}
+		
+		cnt = 0;
+		
+		while(cnt < test_num) {
+			int cnt2 = 0;
+			
+			while(cnt2 < repeat_num.get(cnt)) {
+				
+				System.out.print(repeat_text.get(cnt).charAt(cnt));
+				cnt2++;
+			}
+			System.out.println("");
+			
+			cnt++;
+		}
+
+	}
+
+}

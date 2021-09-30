cace4eb44de765e40fac3524ad59b6312f8f7a6d
SSP_Project <jkijki1249@gmail.com>
2021-09-30 21:16:51 +0900

    백준 2581번

---
+++ Backjoon/src/BackJoon2581.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BackJoon2581 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int start = sc.nextInt();
+		int end = sc.nextInt();
+		sc.close();
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		while(start <= end) {
+			if(start == 1 || start == 0) {
+				start++;
+				continue;
+			}
+			if(calcP(start)) {
+				arr.add(start);
+			}
+			start++;
+		}
+		
+		
+		int i = 0;
+		int result = 0;
+		while(i < arr.size()) {
+			result += arr.get(i);
+			
+			i++;
+					
+		}
+		if(arr.size() == 0) {
+			System.out.println(-1);
+			return;
+		}
+		System.out.println(result);
+		System.out.println(arr.get(0));
+		
+		
+	}
+	
+	public static boolean calcP(int start) {
+		
+		int i = 2;
+		while(i*i <= start) {
+			
+			if(start % i == 0) {
+				return false;
+			}
+			i+=1;
+		}
+		
+		return true;
+	}
+	
+	
+	
+
+}
